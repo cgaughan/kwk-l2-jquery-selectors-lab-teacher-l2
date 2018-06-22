17f85456f1a316931d1fe7bc4fcfdf95d8660606
Caroline Gaughan <cgaughan1122@gmail.com>
2018-06-22 14:02:58 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,2 +1,21 @@
-'use-strict';
+//'use-strict';
 //add your code here
+function h1Selector() {
+  return $("h1");
+}
+
+function liInOlSelector() {
+  return $("ol").children();
+}
+
+function linkSelector() {
+  return $("#box4.box5").children();
+}
+
+function imageSelector() {
+  return $("img[alt='cat sleeping']");
+}
+
+function checkboxInputSelector() {
+  return $("input[type='checkbox']")
+}
